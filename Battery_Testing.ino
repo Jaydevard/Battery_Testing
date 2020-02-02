4d298f4fe03558349fa7165c66d32d8e8c41a38d
kav2017-crypto <girish201r7@gmail.com>
2020-02-01 23:27:20 -0330

    added function read_battery_voltage and constants

--- Battery_Testing.ino
+++ Battery_Testing.ino
@@ -1,11 +1,46 @@
+#include <Chrono.h>
+#include <LightChrono.h>
+#include <rgb_lcd.h>
+
 /* Battery Tester Project 
  *  Created on 1/29/2020
  *  Intended for EE MUN Term 5 Project
  */
 
+Chrono current_read_timer;
 
-#include "rgb_lcd.h"
 
 
 #define Resistor 500
 #define Voltage 5
+#define Current_Sens_Pin A0
+#define Bat_VSensor_Pin A1
+#define RELAY_1 2
+
+
+void isr0(){
+
+  
+}
+
+/**********************************************/
+/*Functions*/
+void check_battery_voltage(){
+  int reading = map(Bat_VSensor_Pin, 0, 1023, 0 ,5);
+  Serial.print("voltage= ");
+  Serial.print(reading);
+  
+}
+
+
+void setup(){
+
+  pinMode(RELAY_1, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+
+  
+}
