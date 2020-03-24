9562e124a25dca03303e07d55652e3dd978fbfd5
Jaydevard <60451362+Jaydevard@users.noreply.github.com>
2020-03-24 20:26:09 -0230

    Completed Code

--- Battery_Testing.ino
+++ Battery_Testing.ino
@@ -9,11 +9,9 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Chrono.h>
-#include <LightChrono.h>
 #include <curveFitting.h>
 #define Current_Sens_Pin A0
 #define Bat_VSensor_Pin A1
-#define Temp_Sensor A2
 #define Current_SensD_Pin A3
 #define Charged_BatVoltage 12.5
 #define MAX_CC 4.0
@@ -48,7 +46,6 @@ bool test_mode = false;
 long int counter = 0;
 bool reset_data = false;
 double internal_res = 0;
-int d_counter = 0;
 
 rgb_lcd lcd;
 Chrono Timer1;
@@ -100,20 +97,16 @@ byte custom_char[8] = {
 /**********************************************/
 /*Functions*/
 /*Function to check the open-circuit battery voltage and terminal voltage*/
-void record_battery_data();     // record the current and voltage readings
-void check_temp();              // check the temperature of the battery
-void setup_discharge();         // setup the discharge rate and cutoff voltage
 void update_battery_status();   // get the battery status
-double calculate_ah_rating(double d_current);
+double calculate_ah_rating(double d_current);   // calculate the Amp Hr rating
 /*Helper Functions****************/
 double get_current();           
 double get_voltage();
 double get_discharge_current();
-void log_data();
+void log_data(bool close_file=false);
 void adjust_discharge_current(bool _dual, double _target_current, bool _reset=false);
 void adjust_charge_current(bool _reset, double target_current);
 void update_battery_status(char _mode, int _counter);
-double calc_Ah_Rating(double d_current);
 /**Screens********************************/    
 void welcome_screen();
 void mode_screen();
@@ -124,6 +117,8 @@ void charging_mode_ongoing_screen();
 void discharging_mode_ongoing_screen();
 
 File check;
+File reader;
+
 void setup(){
   pinMode(C_RELAY, OUTPUT);
   pinMode(D_RELAY, OUTPUT);
@@ -147,21 +142,15 @@ void setup(){
   lcd.createChar(0, cursor_char);
   lcd.createChar(1, custom_char);
   lcd.clear();
+  Serial.begin(9600);
   welcome_screen();
   delay(4000);
-  Serial.begin(9600);
   if(!SD.begin(CS)){
-    Serial.println("Card Failed, or not present!");
+    lcd.setCursor(1,0);
+    lcd.print("SD Fail");     
   }
-  SD.remove("log.csv");
-  check = SD.open("log.csv", FILE_WRITE);
-  if(check){
-    Serial.println("File created");
-    
-  }
-  else{
-    Serial.println("Card Fail");
-  }
+  //SD.remove("log.txt");
+  //check = SD.open("log.txt", FILE_WRITE);
   mode_screen();
   lcd.setCursor(0, 0);
   lcd.write((uint8_t)0);
@@ -382,11 +371,7 @@ void loop(){
       }
    break;
 
-  case 5: // Display charge parameters screen
-   break;
-  
-  
-   }
+ }
  TurnDetected = false;
  }
 
@@ -432,9 +417,9 @@ void loop(){
        case 1: // case for testing_param_screen(Start Button)
        {
         test_mode = true;
-        /*
+        
+        counter =0;
         double current = 0;
-        int counter = 0;
         charging_mode_ongoing_screen();
         lcd.setCursor(11,1);
         lcd.write((uint8_t)0);
@@ -452,6 +437,10 @@ void loop(){
         bool charge_complete = false;
         digitalWrite(C_RELAY, HIGH);
         adjust_charge_current(true, current);
+        open_circuit_voltage = get_voltage();
+        if(open_circuit_voltage > Charged_BatVoltage){
+          charge_complete = true;
+        }
         bool set_voltage = false;
         bool break_out = false;
         button = false;
@@ -530,15 +519,18 @@ void loop(){
         if(break_out){
           break;
         }
-        */
+        
         double d_current = 0;
         TCCR1B = TCCR1B & B11111000 | B00000011;
-        bool break_out = false;
+        break_out = false;
         bool dual = false;
+        bool first_read = true;
+        double first_voltage = 0;
+        double last_voltage = 0;
         lcd.write((uint8_t)0);
         counter = 0;
         if(test_mode){
-          d_current = Ah_rating/2.0;
+          d_current = Ah_rating/3.0;
         }
         else{
           d_current = discharge_current;
@@ -560,20 +552,22 @@ void loop(){
         lcd.setCursor(2,1);
         lcd.print("Discharge");
         internal_res = calculate_internal_res(d_current);
-        Serial.println(internal_res);
+        lcd.clear();
+        lcd.setCursor(1,0);
+        lcd.print("Int Res:");
+        lcd.setCursor(2,1);
+        lcd.print(internal_res);
         reset_data = true;
-        double target_discharge_voltage =11.0; //calculate_cutoff_voltage(d_current);
+        double target_discharge_voltage = 11.0;
         if(d_current > 3){
           target_discharge_voltage = 11.0;
         }
         else if(d_current > 1.5){
-          target_discharge_voltage = 11.5;
+          target_discharge_voltage = 11.50;
         }
         else{
           target_discharge_voltage = 11.7;
         }
-        Serial.println(d_current);
-        Serial.println(target_discharge_voltage);
         delay(2000);
         discharging_mode_ongoing_screen();
         lcd.setCursor(11,1);
@@ -597,7 +591,12 @@ void loop(){
             update_battery_status('d', counter);
           }
           if(Timer3.hasPassed(30)){
+            if(first_read){
+              first_voltage = get_voltage();
+              first_read = false;
+            }
             log_data();
+            last_voltage = get_voltage();
             if(get_voltage() - 0.1 < target_discharge_voltage){
               discharge_complete = true;
             }
@@ -608,6 +607,7 @@ void loop(){
             TurnDetected = false;
             Timer1.stop();
             Timer2.stop();
+            Timer3.stop();
             digitalWrite(D_RELAY, LOW);
             while(button == false){
               delay(10);
@@ -620,6 +620,7 @@ void loop(){
               if(button){
                 Timer1.resume();
                 Timer2.resume();
+                Timer3.resume();
                 digitalWrite(D_RELAY, HIGH);
                 button = false;
                 break;
@@ -638,6 +639,8 @@ void loop(){
             delayMicroseconds(100);
             analogWrite(MOSF2, 0);
             delay(2000);
+            
+            log_data(true);
             digitalWrite(D_RELAY, LOW);
             delay(1000);
             lcd.clear();
@@ -646,7 +649,7 @@ void loop(){
             lcd.setCursor(3,1);
             lcd.print("Complete");
             delay(3000);
-            double ahrating = calculate_ah_rating(d_current);
+            double ahrating = calculate_ah_rating(d_current, counter,first_voltage, last_voltage);
             lcd.clear();
             lcd.setCursor(3,0);
             lcd.print("Test");
@@ -660,7 +663,7 @@ void loop(){
             lcd.print(ahrating);
             while(!button);
             button = false;
-            screen =1;
+            screen = 1;
             TurnDetected = true;
             break;
           }
@@ -710,6 +713,7 @@ void loop(){
         }
         Timer1.start();
         Timer2.start();
+        Timer3.start();
         digitalWrite(C_RELAY, HIGH);
         adjust_charge_current(true, current);
         charge_current = current;
@@ -749,6 +753,7 @@ void loop(){
             TurnDetected = false;
             Timer1.stop();
             Timer2.stop();
+            Timer3.stop();
             digitalWrite(C_RELAY, LOW);
             while(button == false){
               if(TurnDetected){
@@ -760,6 +765,7 @@ void loop(){
               if(button){
                 Timer1.resume();
                 Timer2.resume();
+                Timer3.resume();
                 digitalWrite(C_RELAY, HIGH);
                 button = false;
                 break;
@@ -1036,11 +1042,14 @@ void discharging_mode_ongoing_screen(){
   lcd.print("Back");
 }
 
+
+// Function to get the current
 double get_current(){
   double current = ((analogRead(Current_Sens_Pin)*5000.0/1023.00)-CC_REG_OFFSET)/100;
   return current;
 }
 
+// Function to get the discharge current
 double get_discharge_current(){
   double current = ((analogRead(Current_SensD_Pin)*5000.0/1023.00)-DC_REG_OFFSET)/100;
   current = current - 0.35; // calibration offset
@@ -1048,12 +1057,18 @@ double get_discharge_current(){
  
 }
 
+// Function to get the battery voltage
 double get_voltage(){
   double voltage = ((5.0*analogRead(Bat_VSensor_Pin))/1023.0)*3.04;
   return (voltage);
 }
 
-void log_data(){
+// Function to log the data to SD card every 30 second
+void log_data(bool close_file = false){
+  if(close_file){
+    check.close();
+    return;
+  }
   String data = "";
   static bool starting = true;
   if(starting or reset_data){
@@ -1064,8 +1079,7 @@ void log_data(){
   data += String(counter);
   data += ",";
   data += String(get_voltage());
-  data += ",";
-  data += String(get_discharge_current());
+  data += ";";
   if(check){
     check.println(data);
     Serial.println(data);
@@ -1073,12 +1087,12 @@ void log_data(){
     
   }
   else{
-    Serial.println("Error");
-    return;
+    return -1;
   }
   
 }
 
+// parses the csv fields obtained from each dataline
 String parsecsv(String& input, int& start_pos, char delim ){
   
   if(start_pos == -1){
@@ -1096,6 +1110,7 @@ String parsecsv(String& input, int& start_pos, char delim ){
   }
 }
 
+// calculates the internal resistance of the battery
 double calculate_internal_res(double d_current){
   delay(3000);
   while(true){
@@ -1108,13 +1123,12 @@ double calculate_internal_res(double d_current){
   delay(4000);
   double term_voltage = get_voltage();
   double internal_res = (12.0 - term_voltage)/d_current;
-  Serial.println(internal_res);
   return internal_res;
 }
 
 
 
-
+// helper function to update the LCD screen every predetermined time 
 void update_battery_status(char _mode, int _counter){
   double current = 0.0;
   if (_mode == 'd'){
@@ -1155,6 +1169,7 @@ void update_battery_status(char _mode, int _counter){
  
 }
 
+// Function to help keep the discharge current constant
 void adjust_discharge_current(bool _dual, double _target_current, bool _reset=false){
   static int PWM_9 = 0;
   static int PWM_5 = 0;
@@ -1219,6 +1234,7 @@ void adjust_discharge_current(bool _dual, double _target_current, bool _reset=fa
 }
 
 
+// Function to help keep the charging current constant
 void adjust_charge_current(bool _reset, double target_current){
   static int PWM_10 = 0;
   if(_reset || target_current == 0.0){
@@ -1231,68 +1247,22 @@ void adjust_charge_current(bool _reset, double target_current){
 }
 
 
-
-double calculate_ah_rating( double d_current){
-  check.close();
-  String dataline = "";
-  double bat_voltage = 12.0 - (internal_res*d_current);
-  int counter = 0;
-  int C_rating = 20;
-  double peukerts_const = 1.4;
-  int len = 0;
-  File data = SD.open("log.csv");
-  if(data){
-    while(data.available() != 0){
-      dataline = data.readStringUntil('\n');
-      if(dataline != ""){
-        counter++;
-      }
-    }
-    Serial.print("Counter is ");
-    Serial.println(counter);
-    len = counter;
-    double t[len];
-    double v[len];
-    counter = 0;
-    data.seek(0);
-    Serial.print("Starting to read");
-    while(data.available()){
-      dataline = data.readStringUntil('\n');
-      Serial.println(dataline);
-      dataline.trim();
-      if(dataline != ""){
-        int start_pos = 0;
-        while(start_pos != -1){
-          t[counter] = (parsecsv(dataline, start_pos, ',')).toInt();
-          v[counter] = (parsecsv(dataline, start_pos, ',')).toDouble();
-          counter++;
-          break;
-        }
-      }
-    }
-    data.close();
-    //Serial.println("Starting to read");
-  int orderOfEqn = 2;
+// Function to calculate the Amp Hour Rating of the battery
+double calculate_ah_rating( double d_current, int last_counter, double first_voltage, double last_voltage){
+  double t[2] = {29, last_counter};
+  double v[2] = {first_voltage, last_voltage};
+  double multiplier = (12 - first_voltage)+1.25;
+  double bat_voltage = 11.0 - (d_current*internal_res);
+  int orderOfEqn = 1;
   double coeffs[orderOfEqn+1];
   int ret = fitCurve(orderOfEqn, sizeof(t)/sizeof(double), v, t, sizeof(coeffs)/sizeof(double), coeffs);
-  Serial.println(ret);
   if(ret == 0){
-    double voltage_cutoff_time = coeffs[0]*pow(bat_voltage,2) + coeffs[1]*pow(bat_voltage,1) + coeffs[2];
-    Serial.print("Cutoff time is" );
-    Serial.println(voltage_cutoff_time);
-    double ah_rating = pow(((voltage_cutoff_time/3600)/(C_rating*1.0)), (1/peukerts_const))*(d_current*(C_rating));
-    Serial.println(ah_rating);
-    return ah_rating;
+    double voltage_cutoff_time = coeffs[0]*bat_voltage + coeffs[1];
+    double ah_rating = pow(((voltage_cutoff_time/3600.0)/(C_rating*1.0)), (1/peukerts_const))*(d_current*(C_rating*1.0));
+    return ah_rating*multiplier;
   }
- 
-  }
-  else{
-    Serial.println("Bad Reading");
-    return;
-  }
-  
- 
 };
+ 
 
 
 
